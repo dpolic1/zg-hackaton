fbbcca2a94fe40343f4e7724bf571f6e7f371eb9
Maro54 <Nikay54@gmail.com>
2024-11-23 17:05:26 +0100

    Static mapa dodana u pregled evenata

--- src/features/map/components/GoogleMap.tsx
+++ src/features/map/components/GoogleMap.tsx
@@ -1,35 +1,28 @@
-import { useEffect } from "react";
 
 
 interface MapLoader {
-  isLoaded: boolean;
   eventLocation: {lat : number, lng : number}
 }
 
 export const Map = ( {eventLocation }: MapLoader) => {
 
-  const mapSrc = `https://www.google.com/maps/embed/v1/view?key=AIzaSyB5p476Apq41dAgr2zCIlNue2RQCXSaXws&center=${eventLocation.lat},${eventLocation.lng}&zoom=${12}`;
-  const mapUrl = `https://www.google.com/maps/?q=${eventLocation.lat},${eventLocation.lng}`;
-
-  useEffect(()=>{
-    console.log(eventLocation)
-  })
+  const apiKey = 'AIzaSyB5p476Apq41dAgr2zCIlNue2RQCXSaXws'
+  const mapSrc = `https://maps.googleapis.com/maps/api/staticmap?center=${eventLocation.lat},${eventLocation.lng}&
+  zoom=${12}&size=600x300&markers=${eventLocation.lat},${eventLocation.lng}&key=${apiKey}`;
+  const googleMapsUrl = `https://www.google.com/maps/?q=${eventLocation.lat},${eventLocation.lng}`;
 
   return (
       <div style={{ width: "100%", height: "500px" }}>
-       <a href={mapUrl} target="_blank" rel="noopener noreferrer">
-        <iframe
+      
+      {/* The image of the static map */}
+     
+        <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer">
+        <img
           src={mapSrc}
-          style={{
-            border: 0,
-            width: "100%",
-            height: "100%",
-          }}
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          title="Google Maps"
+          alt="Static Map"
+          style={{ width: "100%", height: "100%" }}
         />
-      </a>
+        </a>
     </div>
   );
 };
